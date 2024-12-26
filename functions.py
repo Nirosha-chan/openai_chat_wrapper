0ea96e0ea8d44b0eeb735604b0a4d22155d89565
Nirosha-chan <nirosha.chanchaiah@themindx.com>
2024-12-26 12:00:10 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+example_functions = [
+    {
+        "name": "example_function",
+        "description": "An example function to demonstrate function calling.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "param1": {"type": "string", "description": "The first parameter."},
+                "param2": {"type": "integer", "description": "The second parameter."}
+            },
+            "required": ["param1"]
+        }
+    }
+]
